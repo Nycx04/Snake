ae8efe7ff1f1760efceacc92048636cf828801b3
Nycx04 <johndavecordoto04@gmail.com>
2026-07-20 23:29:24 +0800

    Add files via upload

---
+++ List.py
@@ -0,0 +1,11 @@
+ShoppiingList = []
+
+addlist = input("Add an item to the list? : ")
+
+ShoppiingList.append(addlist)
+print(ShoppiingList)
+
+RemoveList = input("Remove something from the list? ")
+ShoppiingList.remove(RemoveList)
+
+print(ShoppiingList)
