798cd120166f57308cf99081442744a7de258613
Nycx04 <johndavecordoto04@gmail.com>
2026-07-22 00:23:18 +0800

    Update List.py

--- List.py
+++ List.py
@@ -1,11 +1,22 @@
-ShoppiingList = []
+List = []
+while True:
+ print("=====To Do List=====")
 
-addlist = input("Add an item to the list? : ")
+ print("1. Add Task")
+ print("2. Remove Task")
+ print("3. View Task")
+ print("4. Exit")
 
-ShoppiingList.append(addlist)
-print(ShoppiingList)
 
-RemoveList = input("Remove something from the list? ")
-ShoppiingList.remove(RemoveList)
+ choice = int(input("Choose: "))
 
-print(ShoppiingList)
+ if choice == 1 : 
+  add = input("Enter the task to add: ")
+  List.append(add)
+ elif choice == 2 :
+  remove = input("Which task to delete?: ")
+  List.remove(remove)
+ elif choice == 3 :
+  print(f"{List}")
+ elif choice == 4 :
+  break
